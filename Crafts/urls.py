0812da77889bba55fdb20199be311a24f322696a
Isaiah Shakir <isaiahshakir02@gmail.com>
2025-03-25 17:13:51 -0400

    Code that allows index to show

---
+++ Crafts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+import Crafts
+urlpatterns = [
+    #creating a path for the Craft home page
+    #links to views file for rendering a template for the set up of this page
+    path('', views.index, name='Crafts.index'),
+
+
+]
