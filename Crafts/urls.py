5ab2710607e92ae26ff70515f631c05d97cacd8d
Isaiah Shakir At Work <isaiahshakir02@gmail.com>
2025-03-26 15:42:50 -0400

    Refactored code (names), and removed pycache, and have temporary solution to template for index

--- Crafts/urls.py
+++ Crafts/urls.py
@@ -2,7 +2,7 @@ from django.http import request
 from django.urls import path
 from . import views
 
-import crafts
+import Crafts
 urlpatterns = [
     #creating a path for the Craft home page
     #links to views file for rendering a template for the set up of this page
