91cd11e57724fe1b3ef1fe618bf1d4c5cdf81eb8
Pjones97 <168655932+Pjones97@users.noreply.github.com>
2025-03-26 23:57:40 -0400

    views, url, files and home page/chatbot page created
    created basic pages and link between them

--- Crafts/views.py
+++ Crafts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'Crafts/index.html')
+
+#function renders the Crafts/bot.html template found in Crafts/templates/Crafts
+def bot(request):
+    return render(request, 'Crafts/bot.html')
