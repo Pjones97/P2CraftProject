472a2f301b45fc10f3f1b99bd36ba8706b1e64b0
Isaiah Shakir <isaiahshakir02@gmail.com>
2025-04-15 18:10:00 -0400

    Added videos actually now

--- Media/models.py
+++ Media/models.py
@@ -36,6 +36,7 @@ class CraftIdeaModel(models.Model):
     # genre = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='movie_images/', null=True, blank=True)
+    video = models.FileField(upload_to='movie_videos/', null=True, blank=True)
 
     # rating = models.DecimalField(max_digits=3, default=1, decimal_places=2)
     def __str__(self):
