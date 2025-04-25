3adda4e1982d2855a5e7955db9fd48358c258cb7
Jedaiah-Hudson <jdhud24@gmail.com>
2025-04-25 17:54:18 -0400

    Open Ai is now up and running
    Open AI is now up and running. before running it. type this  "pip install openai" into ur terminal  so that it can work.

--- chatBot/models.py
+++ chatBot/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+		user_message = models.TextField()
+		bot_message = models.TextField()
+		timestamp = models.DateTimeField(auto_now_add=True)
