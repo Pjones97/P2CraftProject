9db1b181b116ae7edb6a1cd26cb5f091dec4189f
Isaiah Shakir <isaiahshakir02@gmail.com>
2025-04-27 16:40:18 -0400

    Update wsgi.py

--- CraftSite/wsgi.py
+++ CraftSite/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P2CraftProject.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CraftSite.settings')
 
 application = get_wsgi_application()
