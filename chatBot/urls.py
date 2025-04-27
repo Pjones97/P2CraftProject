0d3e3a39ea0cb6e2c7d73e1717ec82be1c13fccd
Jedaiah-Hudson <jdhud24@gmail.com>
2025-04-27 03:20:46 -0400

    Added "delete conversation" button

--- chatBot/urls.py
+++ chatBot/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='chatBot.index'),
     path("api/youtube/", views.fetch_youtube_videos, name="youtube_videos"),
-    path('openai/', views.chat_view, name='chat_view'), 
+    path('openai/', views.chat_view, name='chat_view'),
+    path('delete_conversation/', views.delete_conversation, name='delete_conversation'),
 ]
